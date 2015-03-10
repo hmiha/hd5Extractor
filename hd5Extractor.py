a01cca20bb1f5f17fb559c17b9660f9d77d4aebe
hmiha <hajimemihara1108@gmail.com>
2015-03-10 21:51:18 +0900

    rm *.app

--- hd5Extractor.py
+++ hd5Extractor.py
@@ -12,22 +12,18 @@ disparity_scale = 5.
 def load_h5(h5name):
     try:
         h5file = h5py.File(h5name, 'r')
-        #print argvs[1], 'is successfully loaded.'
         return h5file
 
     except IOError:
-        #print 'IOError:', argvs[1], 'is NONE.'
         sys.exit()
 
 def read_and_save(item, data, saveDir, i, n1):
-    l = len(data.shape)         # 4 corresponds to a depthmap, 5 corresponds to LFs
-
+    l = len(data.shape)  # 4 corresponds to a depthmap, 5 corresponds to LFs
     x = i % n1
     y = (i - x) / n1
 
     if l == 4:
         img = np.array(data[y, x, :, :]) * disparity_scale
-
     elif l == 5:
         raw_img = np.array(data[y, x, :, :, :])
         # swap RGB to GBR
@@ -35,7 +31,6 @@ def read_and_save(item, data, saveDir, i, n1):
         img[:, :, 0] = raw_img[:, :, 2].copy()
         img[:, :, 1] = raw_img[:, :, 1].copy()
         img[:, :, 2] = raw_img[:, :, 0].copy()
-
     else:
         return 'Unknown filetype...'
         sys.exit()
